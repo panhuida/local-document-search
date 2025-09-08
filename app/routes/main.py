160f9c4eef8b1891be4ff447aa5f9d08bcdd7d1e
panhuida <panhuida@qq.com>
2025-09-08 22:12:32 +0800

    增加处理失败记录的展示页面

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from app.models import ConversionError
 
 bp = Blueprint('main', __name__)
 
@@ -13,3 +14,8 @@ def convert_page():
 @bp.route('/search')
 def search_page():
     return render_template('search.html')
+
+@bp.route('/errors')
+def errors_page():
+    errors = ConversionError.query.order_by(ConversionError.created_at.desc()).all()
+    return render_template('errors.html', errors=errors)
